2e687ba58b1b4f293cc4df130909e4f1f310e4df
Patryk Pastuszka <nov11157@gmail.com>
2025-05-19 01:43:47 +0200

    Small Bluetooth Updates

---
+++ Utility/CharacteristicDataExchangeAwaiter.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using IRIS.Bluetooth.Common.Abstract;
+
+namespace IRIS.Bluetooth.Common.Utility
+{
+    public sealed class CharacteristicDataExchangeAwaiter(
+        byte[] dataToTransmit,
+        IBluetoothLECharacteristic txCharacteristic,
+        IBluetoothLECharacteristic rxCharacteristic,
+        Predicate<byte[]>? notificationCriteria = null,
+        CancellationToken cancellationToken = default
+    ) : INotifyCompletion
+    {
+        private byte[]? _receivedData;
+        public bool IsCompleted { get; private set; }
+        private Action _continuation = () => { };
+
+        public byte[]? GetResult() => _receivedData;
+
+        public async void OnCompleted(Action continuation)
+        {
+#if DEBUG
+            Debug.Assert(characteristic.IsNotify);
+#endif
+
+            _continuation = continuation;
+
+            // Ensure characteristic is subscribed
+            await rxCharacteristic.SubscribeAsync(cancellationToken);
+            rxCharacteristic.ValueChanged += OnCharacteristicValueChanged;
+
+            // Transmit data
+            await txCharacteristic.WriteAsync(dataToTransmit, cancellationToken);
+        }
+
+        private void OnCharacteristicValueChanged(
+            IBluetoothLECharacteristic bluetoothLECharacteristic,
+            byte[] newValue)
+        {
+            // Check if notification criteria is valid
+            if (notificationCriteria != null && !notificationCriteria(newValue)) return;
+
+            // Unsubscribe
+            rxCharacteristic.ValueChanged -= OnCharacteristicValueChanged;
+
+            // Handle data
+            _receivedData = newValue;
+            IsCompleted = true;
+            _continuation();
+        }
+    }
+}
